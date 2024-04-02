ca53b4d527865c2a3c7dfe67ba31a0d1a95b135e
Vishal <vishalpanchal1828vp@gmail.com>
2024-04-03 03:35:27 +0530

    connected liveblock and added skeleton for board loading

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,4 +1,7 @@
+import { Room } from "@/components/room";
+
 import { Canvas } from "./_components/convas";
+import { Loading } from "./_components/loading";
 
 interface BoardIdPageProps {
   params: {
@@ -7,7 +10,11 @@ interface BoardIdPageProps {
 }
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return <Canvas boardId={params.boardId} />;
+  return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
 };
 
 export default BoardIdPage;

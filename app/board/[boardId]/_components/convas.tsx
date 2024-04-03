eba7c32a695cd07e35650b0c8a9603372fd6decf
Vishal <vishalpanchal1828vp@gmail.com>
2024-04-04 03:04:27 +0530

    setup onclick on toolbar and handle type as well

--- app/board/[boardId]/_components/convas.tsx
+++ app/board/[boardId]/_components/convas.tsx
@@ -1,6 +1,9 @@
 "use client";
+import { useState } from "react";
 
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
 import { useSelf } from "@/liveblocks.config";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
 import { Info } from "./info";
 import { Participants } from "./participants";
@@ -11,12 +14,26 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
   const info = useSelf((me) => me.info);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
